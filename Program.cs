05f962505e3995334bede0e947c08be102716222
Softygrangy <129508225+Softygrangy@users.noreply.github.com>
2023-06-02 14:26:21 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,54 +1,63 @@
-﻿int[,] table = new int[3, 4];
-FillArrayRandom(table);
-PrintArray(table);
-SortToLower(table);
-Console.WriteLine();
-PrintArray(table);
+﻿Console.WriteLine("Введите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
 
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
 
-
-void FillArrayRandom(int[,] array)
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
 {
- for (int i = 0; i < array.GetLength(0); i++)
-    {
-    for (int j = 0; j < array.GetLength(1); j++)
-        {
-        array[i, j] = new Random().Next(1, 10);
-        }
-    }
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
 }
 
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
 
-void SortToLower(int[,] array)
+
+int SumLineElements(int[,] array, int i)
 {
- for (int i = 0; i < array.GetLength(0); i++)
-    {
-    for (int j = 0; j < array.GetLength(1); j++)
-        {
-        for (int k = 0; k < array.GetLength(1) - 1; k++)
-            {
-            if (array[i, k] < array[i, k + 1])
-                {
-                int temp = array[i, k + 1];
-
-                        array[i, k + 1] = array[i, k];
-
-                                     array[i, k] = temp;
-                }
-            }
-        }
-    }
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
 }
 
-
-void PrintArray(int[,] array)
+int InputNumbers(string input)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
+      array[i, j] = new Random().Next(range);
     }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
 }
